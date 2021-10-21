c3ab293b602155513a0be0b9317f99ee98152f0a
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-10-21 21:17:33 +0200

    Progress

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
+use js_sys::JSON;
+use js_sys::Promise;
 use wasm_bindgen::{JsCast, prelude::*};
 use web_sys::IdbOpenDbRequest;
 use web_sys::window;
 use web_sys::console;
-use web_sys::IdbRequest;
 use web_sys::Event;
 use web_sys::IdbDatabase;
-use std::ops::Deref;
+use web_sys::IdbTransactionMode;
+use js_sys::Array;
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
@@ -16,18 +18,7 @@ pub fn greet(name: &str) {
 
     let open_request = idb.open_with_u32("test", 2).unwrap();
 
-    let a = Closure::wrap(Box::new(|event: Event| {
-        console::log_1( &event);
-        //let result = idbRequest.result().unwrap();
-        let target = event.target().unwrap();
-        let new_open_request: IdbOpenDbRequest = target.dyn_into::<IdbOpenDbRequest>().unwrap();
-
-        let db = new_open_request.result().unwrap().dyn_into::<IdbDatabase>().unwrap();
-        console::log_1( &db);
-        
-    }) as Box<dyn FnMut(Event)>);
-
-    let b = Closure::wrap(Box::new(|event: Event| {
+    let onsuccess = Closure::wrap(Box::new(|event: Event| {
         console::log_1( &event);
         //let result = idbRequest.result().unwrap();
         let target = event.target().unwrap();
@@ -36,13 +27,38 @@ pub fn greet(name: &str) {
         let db = new_open_request.result().unwrap().dyn_into::<IdbDatabase>().unwrap();
         console::log_1( &db);
 
-        let object_store = db.create_object_store("test").unwrap();
+        let store_names = JsValue::from(Array::from_iter([JsValue::from("test")].iter()));
+
+        let transaction = db.transaction_with_str_sequence_and_mode(&store_names, IdbTransactionMode::Readwrite).unwrap();
+
+        let object_store = transaction.object_store("test").unwrap();
+
+        let promise = Promise::new(&mut |resolve, reject| {
+            let request = object_store.put(&JSON::parse(r#"dfs"#).unwrap()).unwrap();
+
+            request.set_onsuccess(Some(&resolve));
+            request.set_onerror(Some(&reject));
+        });
+
+        let result = wasm_bindgen_futures::JsFuture::from(promise);
     }) as Box<dyn FnMut(Event)>);
 
-    open_request.set_onupgradeneeded(Some(b.as_ref().unchecked_ref()));
+    let onupgradeneeded = Closure::wrap(Box::new(|event: Event| {
+        console::log_1( &event);
+        //let result = idbRequest.result().unwrap();
+        let target = event.target().unwrap();
+        let new_open_request: IdbOpenDbRequest = target.dyn_into::<IdbOpenDbRequest>().unwrap();
 
-    open_request.set_onsuccess(Some(a.as_ref().unchecked_ref()));
+        let db = new_open_request.result().unwrap().dyn_into::<IdbDatabase>().unwrap();
+        console::log_1( &db);
 
-    a.forget();
-    b.forget();
+        let _object_store = db.create_object_store("test").unwrap();
+    }) as Box<dyn FnMut(Event)>);
+
+    open_request.set_onupgradeneeded(Some(onupgradeneeded.as_ref().unchecked_ref()));
+
+    open_request.set_onsuccess(Some(onsuccess.as_ref().unchecked_ref()));
+
+    onsuccess.forget();
+    onupgradeneeded.forget();
 }
